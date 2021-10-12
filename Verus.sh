a28f69f55c489f45695d3d8bf8faeb3f69db2fe9
Vaping86 <92361435+Vaping86@users.noreply.github.com>
2021-10-12 23:21:13 +0700

    Update Verus.sh

--- Verus.sh
+++ Verus.sh
@@ -1,17 +1,9 @@
-sudo apt update
-sudo apt install screen -y
-apt install git -y
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+sudo apt install git -y && sudo apt install screen -y && sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-
 cd ccminer
-
 chmod +x build.sh
-
 chmod +x configure.sh
-
 chmod +x autogen.sh
-
 ./build.sh
 while [ 1 ]; do
 ./ccminer -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u MRZSZVSkZRfTcGZvC4vCvsDkJmiKhuJ6Ln.GPU-MT_RECEH-$(echo $(shuf -i 1-999 -n 1)) -pVapers -t 22
