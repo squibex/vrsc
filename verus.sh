da0977c3e0e7363db7fbd8ecd9b9e0208c0e53a7
squibex <90225595+squibex@users.noreply.github.com>
2021-11-24 01:30:06 -0800

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,4 +1,4 @@
-sudo apt install git -y && sudo apt install screen -y && sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+sudo apt update && sudo apt install git screen -y && sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
